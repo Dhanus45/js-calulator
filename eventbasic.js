f9bdd5249de4eb32ab795850fb08047e7864ccba
Dhanus45 <133327430+Dhanus45@users.noreply.github.com>
2024-09-17 17:06:14 +0530

    Add files via upload

---
+++ eventbasic.js
@@ -0,0 +1,27 @@
+const dis= document.getElementById("display")
+
+function append(input){
+    dis.value +=input;
+}
+   
+
+
+function calul(){
+   
+        dis.value=eval(dis.value);
+        if(dis.value== null)
+        {
+           dis.value='divide by 0 error';
+        }else
+        {
+            document.getElementById('demo').innerHTML=' ';
+ 
+        }
+
+} 
+
+
+function clean(){
+    dis.value=" ";
+    document.getElementById('demo').innerHTML=' ';
+}
